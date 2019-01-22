c4b0e3d9ed8018400047f5c280f34493f0b6f4dc
jiefangxuanyan <505745416@qq.com>
2019-01-22 15:10:14 +0800

    代码有bug啊

--- run_tuning.py
+++ run_tuning.py
@@ -781,5 +781,5 @@ if __name__ == "__main__":
     flags.mark_flag_as_required("data_dir")
     flags.mark_flag_as_required("vocab_file")
     flags.mark_flag_as_required("bert_config_file")
-    flags.mark_flag_as_required("output_dir")
+    flags.mark_flag_as_required("output_dirs_path")
     tf.app.run()
